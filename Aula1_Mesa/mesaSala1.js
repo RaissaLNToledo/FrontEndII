8d52aae6ef1de1b64f58960bc619121d1223bbe8
RaissaLNToledo <raissaltoledo@yahoo.com.br>
2023-02-13 22:54:08 -0300

    Mesa de Trabalho equipe 1

---
+++ Aula1_Mesa/mesaSala1.js
@@ -0,0 +1,15 @@
+//Equipe 1:
+//Erick rodrigo cardoso
+//Felipe Levy
+//Nicolas Paiffer do Carmo
+//Tomas Ott
+//Wagner Yuri
+//Raíssa da Luz Nogueira de Toledo
+
+let numeros = [1, 2, 4, 8]
+let soma = 0
+
+numeros.forEach(element => {
+   soma = soma + element 
+   console.log(soma);
+});
